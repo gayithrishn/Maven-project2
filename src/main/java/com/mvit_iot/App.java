9a4dda8da33a1eab0fb6cbe204c7c50b18bef9a8
gayithrishn <gayithrishn@gmail.com>
2025-03-25 22:32:48 +0530

    add some additional changes

--- src/main/java/com/mvit_iot/App.java
+++ src/main/java/com/mvit_iot/App.java
@@ -8,6 +8,10 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Hello welcome to devops learning" );
+        for(int i=0;i<=10;i++)
+        {
+            System.out.println("is is:", i);
+        }
     }
 }
